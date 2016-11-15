65f75c455f16aeac3158ef36275ae5ac41481b4a
Matheus Portela <matheus.v.portela@gmail.com>
2016-11-15 16:40:01 -0200

    Enhance card_manager.js style

--- test/card_manager.js
+++ test/card_manager.js
@@ -7,14 +7,14 @@ contract('CardManager', function(accounts) {
   it('should create new card', function() {
     var cardManager = CardManager.deployed();
     cardManager.newCard('Test Card', accounts[0]).then(function() {
-        cardManager.getCard.call(1).then(function(card) {
-            // Since getCard returns two variables, they are stored in a list
-            // and can be retrieved in the same order as they were returned
-            name = card[0];
-            owner = card[1];
+      return cardManager.getCard.call(1);
+    }).then(function(card) {
+      // Since getCard returns two variables, they are stored in a list
+      // and can be retrieved in the same order as they were returned
+      name = card[0];
+      owner = card[1];
 
-            assert.equal(name, 'Test Card', "card wasn't added to cards list");
-        });
+      assert.equal(name, 'Test Card', "card wasn't added to cards list");
     });
   });
 });

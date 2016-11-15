6a404875f07f28574a502dafa3576f2dda05b5c7
Matheus Portela <matheus.v.portela@gmail.com>
2016-11-14 22:39:39 -0200

    Create card manager
    This contract controls which cards were inserted in the blockchain.

---
+++ contracts/CardManager.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.2;
+
+// Contract to manage card creation and transfers
+contract CardManager {
+  Card[] public cards;
+
+  struct Card {
+    uint id;
+    string name;
+    address owner;
+  }
+
+  event NewCard(uint id, string name, address owner);
+
+  function CardManager() {
+    // Create card with ID 0 to represent a null card
+    newCard('null', 0);
+  }
+
+  function newCard(string name, address owner) {
+    uint cardID = cards.length++;
+    Card card = cards[cardID];
+    card.id = cardID;
+    card.name = name;
+    card.owner = owner;
+    NewCard(card.id, card.name, card.owner);
+  }
+
+  function getCard(uint cardID) returns (string name, address owner) {
+    Card card = cards[cardID];
+    name = card.name;
+    owner = card.owner;
+  }
+
+  // Refuse receiving ether in this contract
+  function () {
+    throw;
+  }
+}
